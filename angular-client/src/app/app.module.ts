d3930c7dbe2cf6eb9498db3d36706bec9dc32a83
Italo Brenner <italobrenner.mf@gmail.com>
2018-05-10 23:11:35 -0300

    Adicionar o template responsivo para celular

--- angular-client/src/app/app.module.ts
+++ angular-client/src/app/app.module.ts
@@ -1,16 +1,39 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { RouterModule } from "@angular/router";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
-import { AppComponent } from './app.component';
+import {
+  MatButtonModule,
+  MatCheckboxModule,
+  MatIconModule,
+  MatMenuModule,
+  MatSidenavModule,
+  MatToolbarModule,
+  MatDividerModule,
+  MatListModule
+} from "@angular/material";
+
+import { AppComponent } from "./app.component";
+import { HomeComponent } from "./home/home.component";
+
+import { ROUTES } from "./app.routes";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, HomeComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatIconModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatToolbarModule,
+    MatMenuModule,
+    MatSidenavModule,
+    MatListModule,
+    RouterModule.forRoot(ROUTES)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
