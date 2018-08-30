463fabf2dca4e19c38451a5b58c9eade5fc090c3
Italo Brenner Lauriano de Oliveira <italobrenner.mf@gmail.com>
2018-08-30 09:44:45 -0300

    Criar esboco da pagina publica

--- angular-client/src/app/app-routing.module.ts
+++ angular-client/src/app/app-routing.module.ts
@@ -1,11 +1,36 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { Page1Component } from './page1/page1.component';
+import { LoginComponent } from './user/login/login.component';
+import { CarComponent } from './car/car.component';
+import { BookComponent } from './book/book.component';
+import { PlaneComponent } from './plane/plane.component';
 
 const routes: Routes = [
   {
     path: '',
     component: HomeComponent
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'page1',
+    component: Page1Component
+  },
+  {
+    path: 'cars',
+    component: CarComponent
+  },
+  {
+    path: 'books',
+    component: BookComponent
+  },
+  {
+    path: 'planes',
+    component: PlaneComponent
   }
 ];
 
--- angular-client/src/app/app.module.ts
+++ angular-client/src/app/app.module.ts
@@ -4,17 +4,31 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { AppRoutingModule } from './app-routing.module';
-import { SharedModule } from './shared/shared.module';
+import { TopbarComponent } from './shared/components/topbar/topbar.component';
+import { SidebarComponent } from './shared/components/sidebar/sidebar.component';
+import { Page1Component } from './page1/page1.component';
+import { CarComponent } from './car/car.component';
+import { BookComponent } from './book/book.component';
+import { PlaneComponent } from './plane/plane.component';
+import { UserComponent } from './user/user.component';
+import { LoginComponent } from './user/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    TopbarComponent,
+    SidebarComponent,
+    Page1Component,
+    CarComponent,
+    BookComponent,
+    PlaneComponent,
+    UserComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    SharedModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
